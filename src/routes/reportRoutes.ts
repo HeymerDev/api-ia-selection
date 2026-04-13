4a7f357d727b2bd444daa8d12aa067d1b329fbb3
HeymerDev <heymermeza11@gmail.com>
2026-04-13 15:59:20 -0500

    add verifyToken middleware (auth middleware) protected routes

--- src/routes/reportRoutes.ts
+++ src/routes/reportRoutes.ts
@@ -4,7 +4,11 @@ import {
   handleGetReportes,
 } from "../controllers/reportController.js";
 
+import { verificarToken } from "../middlewares/authMiddleware.js";
+
 const router = Router();
+
+router.use(verificarToken); // Aplica el middleware de autenticación a todas las rutas de este router
 
 router.post("/", handleCreateReporte);
 router.get("/", handleGetReportes);
--- src/routes/yoloRoutes.ts
+++ src/routes/yoloRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 import { handleVideoAnalysis } from "../controllers/yoloController.js";
+import { verificarToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
@@ -9,6 +10,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // El campo en el Body de Postman/Frontend debe ser 'video'
+router.use(verificarToken);
 router.post("/analizar", upload.single("video"), handleVideoAnalysis);
 
 export default router;
